b5b8642b209237098fef0fae1846f8e5b8b817bd
Sean Devenish <sdevenish@collinssba.com.au>
2014-05-13 10:00:01 +1000

    Initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CSBAApps::Application.config.secret_key_base = 'b98c7fe233d9cd1fa0378df68b6fb6264d3b3850138b082f1b1ad095cab2776240861800260d58cfb196eb87ee1ca76eeb165e57f62981e26e21a64923753ecf'
